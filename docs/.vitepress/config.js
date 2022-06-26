86cc19b94daa80473bc4b968deda85112931a600
Dhruv Bhanushali <dhruv_b@live.com>
2022-06-26 12:20:52 +0400

    Add image assets for logo and favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,17 @@
+const base = "/reschume/";
+
 export default {
   title: "Réschumé",
   description: "A comprehensive schema for documenting résumés.",
 
   lang: "en-US",
-  base: "/reschume/",
+  base,
+
+  head: [
+    ["link", { rel: "icon", type: "image/png", href: `${base}favicon.png` }],
+  ],
+
+  themeConfig: {
+    logo: "/logo.svg", // automatically uses `base`
+  },
 };
